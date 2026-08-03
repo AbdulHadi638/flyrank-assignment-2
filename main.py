70a22388690a3b122ba7e95dee58b153279a5f57
hadi <mabdulhadi033@gmail.com>
2026-08-03 18:02:20 +0500

    Stage 2: created different tasks

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
+
+tasks = [
+    {
+        "id": 1,
+        "title": "Learn FastAPI",
+        "done": False
+    },
+    {
+        "id": 2,
+        "title": "Build Task API",
+        "done": False
+    },
+    {
+        "id": 3,
+        "title": "Push to GitHub",
+        "done": True
+    }
+]
 
 @app.get("/")
 def root():
@@ -16,3 +34,20 @@ def health():
     return {
         "status": "ok"
     }
+
+
+@app.get("/tasks")
+def task_list():
+    return tasks
+
+
+@app.get("/tasks/{id}")
+def get_task(id: int):
+    for task in tasks:
+        if task["id"] == id:
+            return task
+
+    raise HTTPException(
+        status_code=404,
+        detail=f"Task {id} not found"
+    )
