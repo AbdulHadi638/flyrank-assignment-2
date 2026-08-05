136eefebf3ede8426fd529d40f34e48c73daf2db
hadi <mabdulhadi033@gmail.com>
2026-08-05 16:16:29 +0500

    Stage 3: update and delete with SQL

--- main.py
+++ main.py
@@ -5,25 +5,8 @@ import sqlite3
 app = FastAPI()
 connection = sqlite3.connect("tasks.db", check_same_thread=False)
 cursor=connection.cursor()
-tasks = [
-    {
-        "id": 1,
-        "title": "Learn FastAPI",
-        "done": False
-    },
-    {
-        "id": 2,
-        "title": "Build Task API",
-        "done": False
-    },
-    {
-        "id": 3,
-        "title": "Push to GitHub",
-        "done": True
-    }
 
-]
-print(tasks)
+
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS tasks(
     id INTEGER PRIMARY KEY,
@@ -123,39 +106,59 @@ def create_task(task: TaskCreate):
 
     
 @app.put("/tasks/{id}")
-def update_task(id: int, updated_task: TaskUpdate):
-
-    for task in tasks:
-
-        if task["id"] == id:
-
-            if updated_task.title.strip() == "":
-                raise HTTPException(
-                    status_code=400,
-                    detail="Title cannot be empty"
-                )
-
-            task["title"] = updated_task.title
-            task["done"] = updated_task.done
-
-            return task
-
-    raise HTTPException(
-        status_code=404,
-        detail=f"Task {id} not found"
+def update_task(id: int, task: TaskUpdate):
+    if task.title.strip() == "":
+     raise HTTPException(
+        status_code=400,
+        detail="Title cannot be empty"
     )
+    cursor.execute(
+        "SELECT * FROM tasks WHERE id = ?",
+        (id,)
+    )
+
+    row = cursor.fetchone()
+
+    if row is None:
+        
+        raise HTTPException(
+            status_code=404,
+            detail=f"Task {id} not found"
+        )
+
+    cursor.execute(
+        "UPDATE tasks SET title = ?, done = ? WHERE id = ?",
+        (task.title, task.done, id)
+    )
+
+    connection.commit()
+
+    return {
+        "id": id,
+        "title": task.title,
+        "done": task.done
+    }
 @app.delete("/tasks/{id}", status_code=204)
 def delete_task(id: int):
 
-    for index, task in enumerate(tasks):
-
-        if task["id"] == id:
-
-            tasks.pop(index)
-
-            return
-
-    raise HTTPException(
-        status_code=404,
-        detail=f"Task {id} not found"
+    cursor.execute(
+        "SELECT * FROM tasks WHERE id = ?",
+        (id,)
     )
+
+    row = cursor.fetchone()
+
+    if row is None:
+        raise HTTPException(
+            status_code=404,
+            detail=f"Task {id} not found"
+        )
+
+    cursor.execute(
+        "DELETE FROM tasks WHERE id = ?",
+        (id,)
+    )
+
+    connection.commit()
+
+    return
